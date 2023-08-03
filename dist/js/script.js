685416bacad199b7342eabd5ac12be61d50c529b
AlcariaJoshua <alcariajoshua13@gmail.com>
2023-08-03 12:35:44 +0800

    tailwind 03/08/23

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,10 @@
+const borger = document.querySelector(".borger");
+const drop = document.querySelector(".drop");
+console.log(borger)
+console.log(drop)
+
+
 borger.addEventListener('click',()=>{
-    drop.classlist.toggle('h-[20rem');
+    drop.classList.toggle('h-[23.3rem]');
+    drop.classList.toggle('overflow-y-scroll');
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,9 @@ module.exports = {
         'gurey':'#2C2C32',
         'dark':'#25252B',
         'asul':'#0066B8',
-        'light':'#ffffff'
+        'light':'#ffffff',
+        'grey':'#a5a5a7',
+        'gray':'#3d3d42'
       },
     },
   },
